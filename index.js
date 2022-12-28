dd6e4d06de13863c3a2c3f0a27c98411cc41a47a
bagble <34789997+bagble@users.noreply.github.com>
2022-12-28 05:19:18 +0000

    Initial commit

--- index.js
+++ index.js
@@ -2,8 +2,38 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+const s = {
+  "Kimsangjun": 135,
+  "Parksunjun": 516,
+  "Kr": 181818,
+  "Test": 9999
+}
+
+app.get('/ok', (req, res) => {
+  res.json({
+    "Scores": s,
+    "code": 200,
+    "message": "OK"
+  })
+})
+
+app.get('/leaderboard', (req, res) => {
+  res.json({
+    "Scores": s,
+    "code": 200,
+    "message": "OK"
+  })
+})
+
+app.post('/leaderboard', (req, res) => {
+  console.log(req.body)
+  res.status(201).json({
+    "code": 201,
+    "message": "Created"
+  })
 })
 
 app.listen(port, () => {
